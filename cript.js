1a3e08fcea59652e411c87e5b520ee0d3d49d752
Biwash Shrestha <biwash@binep.tech>
2025-11-08 08:48:40 +0545

    Well On what

--- cript.js
+++ cript.js
@@ -1,1 +1,8 @@
 let Rationed = 0;
+const table = document.getElementById('memberTable');
+    const assertion = document.getElementById('assertionText');
+
+    const rows = table.querySelectorAll('tbody tr');
+    const count = rows.length;
+
+    assertion.textContent = `Assertion: ${count} members listed upon company prospect at current view.`;
